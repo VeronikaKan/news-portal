f70c7c04ec466536e14f874ef857d987d3e1c6c1
VeronikaKan <kan.v98@gmail.com>
2023-11-01 00:25:44 +0600

    first commit

---
+++ news-portal-back/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const { Pool, Client } = require("pg");
+const _ = require('lodash');
+const multer = require('multer')
+
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) =>{
+      cb(null, "upload");
+  },
+  filename: (req, file, cb) =>{
+      cb(null, file.originalname);
+  }
+});
+app.use(multer({storage:storageConfig}).single("addImage"));
+
+const { router } = require("./routes/routes");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api", router);
+
+app.use('/upload', express.static('upload'));
+
+async function startApp() {
+  try {
+    const http = require("http");
+setInterval(function() {
+    http.get("http://gipgipnews.herokuapp.com/api/log");
+}, 300000); 
+    app.listen(3030, () => {
+      console.log(`app started`);
+    });
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  } finally {
+    console.log("Hello epta");
+  }
+}
+startApp();
+ 
---
+++ news-portal-back/controllers_news/addNews.js
@@ -0,0 +1,75 @@
+const { Pool, Client } = require("pg");
+
+const pool = new Pool({
+  connectionString: `${process.env.connectionString}`
+});
+
+async function addNews(req, res) {
+  const time = req.requestTime;
+  const { author, content, title } = req.body;
+  const filedata = req.file;
+  let nameFile = "ggnews.jpg";
+  if (filedata) {
+    nameFile = filedata.originalname;
+  }
+  if(!author || !title || !content){
+    return res
+    .status(400)
+    .json({ message: "Otsutvuyut obyaz polya" });
+  }
+  if (author.trim() === "") {
+    return res
+      .status(400)
+      .json({ message: "Неправильно заполнено поле author" });
+  }
+  if (title.trim() === "") {
+    return res
+      .status(400)
+      .json({ message: "Неправильно заполнено поле title" });
+  }
+  if (content.trim() === "") {
+    return res
+      .status(400)
+      .json({ message: "Неправильно заполнено поле content" });
+  }
+  const client = await pool.connect();
+  (async () => {
+    try {
+      await client.query("INSERT INTO views(views_count) VALUES($1)", [1]);
+      const viewsId = await client.query(
+        "SELECT currval('views_id_seq'::regclass)"
+      );
+      await client.query("INSERT INTO likes(likes_count) VALUES($1)", [1]);
+      const likeId = await client.query(
+        "SELECT currval('likes_likes_id_seq'::regclass)"
+      );
+      await client.query(
+        "INSERT INTO news(title,date,author,content,likes_id,views_id, image) VALUES($1,$2,$3,$4,$5,$6,$7)",
+        [
+          title,
+          time,
+          author,
+          content,
+          likeId.rows[0]["currval"],
+          viewsId.rows[0]["currval"],
+          `https://gipgipnews.herokuapp.com/upload/${nameFile}`,
+        ]
+      );
+      const newsInfo = await client.query(
+        `select * from news where title='${title}'`
+      );
+      const news = newsInfo.rows[0];
+
+      return res
+        .status(200)
+        .json({ massege: "Новость успешно сохранена", news });
+    } finally {
+      client.release();
+    }
+  })().catch((err) => {
+    console.log(err);
+    return res.status(400).json(err.detail);
+  });
+}
+
+module.exports = { addNews };
